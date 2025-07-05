7c9ddfdb085c296cde5a251f5fcac79a1ea29198
shyam-itspe <shyam.singh@itspe.co.in>
2025-07-05 13:56:48 +0530

    added identify_router in main

---
+++ api/routes/identify.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+from api.services.identify_logic import identify_contact
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s %(message)s")
+logger = logging.getLogger(__name__)
+
+router = APIRouter()
+
+
+class IdentifyRequest(BaseModel):
+    email: str | None
+    phoneNumber: str | None
+
+
+@router.post("/identify")
+def identify(request: IdentifyRequest):
+    try:
+        if not request.email and not request.phoneNumber:
+            raise HTTPException(
+                status_code=400, detail="Either email or phoneNumber is required."
+            )
+
+        return identify_contact(email=request.email, phoneNumber=request.phoneNumber)
+    except Exception as e:
+        logger.error(f"Error identifying contact: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
